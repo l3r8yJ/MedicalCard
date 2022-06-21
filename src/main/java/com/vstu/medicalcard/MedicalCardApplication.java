d96800bf8f2f395be2fc91debc22473432f5fd93
Ivan <clicker.heroes.acg@gmail.com>
2022-06-21 17:56:41 +0300

    Initial commit

---
+++ src/main/java/com/vstu/medicalcard/MedicalCardApplication.java
@@ -0,0 +1,13 @@
+package com.vstu.medicalcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalCardApplication.class, args);
+    }
+
+}
