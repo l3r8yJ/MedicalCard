a653d55009708849c64f91e75944159300f3644e
Ivan <clicker.heroes.acg@gmail.com>
2022-06-21 19:10:35 +0300

    config

---
+++ src/main/java/com/vstu/medicalcard/config/ApplicationConfig.java
@@ -0,0 +1,33 @@
+package com.vstu.medicalcard.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+@ComponentScan("com.vstu.medicalcard")
+@EnableWebMvc
+public class ApplicationConfig implements WebMvcConfigurer {
+    private final ApplicationContext applicationContext;
+
+
+    @Autowired
+    public ApplicationConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: l3r8y
+  Date: 21.06.2022
+  Time: 18:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>MedicalCard</title>
+</head>
+<body>
+
+</body>
+</html>
